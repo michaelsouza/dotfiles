7c1ea321e5998d578de0bbb60401cc7857ec6f20
michael <michael@ufc.br>
2026-06-04 07:50:40 -0300

    Add session persistence

---
+++ lua/plugins/persistence.lua
@@ -0,0 +1,49 @@
+return {
+  {
+    "folke/persistence.nvim",
+    event = "BufReadPre",
+    opts = {
+      dir = vim.fn.stdpath("state") .. "/sessions/",
+      options = {
+        "buffers",
+        "curdir",
+        "tabpages",
+        "winsize",
+        "help",
+        "globals",
+        "skiprtp",
+        "folds",
+      },
+    },
+    keys = {
+      {
+        "<leader>qs",
+        function()
+          require("persistence").load()
+        end,
+        desc = "Restore session",
+      },
+      {
+        "<leader>qS",
+        function()
+          require("persistence").select()
+        end,
+        desc = "Select session",
+      },
+      {
+        "<leader>ql",
+        function()
+          require("persistence").load({ last = true })
+        end,
+        desc = "Restore last session",
+      },
+      {
+        "<leader>qd",
+        function()
+          require("persistence").stop()
+        end,
+        desc = "Do not save session",
+      },
+    },
+  },
+}
