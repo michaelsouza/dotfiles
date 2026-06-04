0ff83945c3c905d928773b1c6d4836c47ec22e4b
michael <michael@ufc.br>
2026-06-04 06:46:57 -0300

    Connect LSP capabilities to autocomplete

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -12,8 +12,23 @@ return {
     dependencies = {
       "mason-org/mason.nvim",
       "neovim/nvim-lspconfig",
+      "hrsh7th/cmp-nvim-lsp",
     },
     config = function()
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+      vim.lsp.config("lua_ls", {
+        capabilities = capabilities,
+      })
+
+      vim.lsp.config("pyright", {
+        capabilities = capabilities,
+      })
+
+      vim.lsp.config("clangd", {
+        capabilities = capabilities,
+      })
+
       require("mason-lspconfig").setup({
         ensure_installed = {
           "lua_ls",
