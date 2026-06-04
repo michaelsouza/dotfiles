5ffc2c5319d1b123922001819f592b2b897425c8
michael <michael@ufc.br>
2026-06-04 06:42:57 -0300

    Add Mason and LSP configuration

---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,52 @@
+return {
+  {
+    "mason-org/mason.nvim",
+    cmd = "Mason",
+    config = function()
+      require("mason").setup()
+    end,
+  },
+
+  {
+    "mason-org/mason-lspconfig.nvim",
+    dependencies = {
+      "mason-org/mason.nvim",
+      "neovim/nvim-lspconfig",
+    },
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = {
+          "lua_ls",
+          "pyright",
+          "clangd",
+        },
+        automatic_enable = true,
+      })
+
+      vim.diagnostic.config({
+        virtual_text = true,
+        signs = true,
+        underline = true,
+        update_in_insert = false,
+        severity_sort = true,
+      })
+
+      vim.api.nvim_create_autocmd("LspAttach", {
+        callback = function(event)
+          local map = vim.keymap.set
+          local opts = { buffer = event.buf }
+
+          map("n", "gd", vim.lsp.buf.definition, opts)
+          map("n", "gD", vim.lsp.buf.declaration, opts)
+          map("n", "gr", vim.lsp.buf.references, opts)
+          map("n", "K", vim.lsp.buf.hover, opts)
+          map("n", "<leader>rn", vim.lsp.buf.rename, opts)
+          map("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+          map("n", "<leader>df", vim.diagnostic.open_float, opts)
+          map("n", "[d", vim.diagnostic.goto_prev, opts)
+          map("n", "]d", vim.diagnostic.goto_next, opts)
+        end,
+      })
+    end,
+  },
+}
